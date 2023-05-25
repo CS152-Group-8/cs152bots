adb87b80132f7d7984b942d8087ae1314b767b48
Kamran Ahmed <k.ahmed@berkeley.edu>
2023-05-24 20:40:43 -0700

    Disable link previous for self help resources

--- DiscordBot/messages.py
+++ DiscordBot/messages.py
@@ -120,9 +120,9 @@ class ReportDetailsMessage:
         " following helplines as they will be able to provide you with immediate"
         " support and resources:\n- Emergency: 911\n- Self-Harm Hotline: 1-800-DONT-CUT"
         " (1-800-366-8288)\n- Crisis Text Line: Text 'HOME' to 741741 or"
-        " https://connect.crisistextline.org/chat\n- National Suicide Prevention"
+        " <https://connect.crisistextline.org/chat>\n- National Suicide Prevention"
         " Lifeline: 1-800-273-TALK (8255) or"
-        " https://suicidepreventionlifeline.org/chat/"
+        " <https://suicidepreventionlifeline.org/chat/>"
     )
     ADDITIONAL_INFO = (
         "Would you like to provide any additional information? Please type `yes` or"
